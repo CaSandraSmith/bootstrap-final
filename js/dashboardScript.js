58ac031e7b4e45d426e53718c654f72dfc8b5329
Ca'Sandra Smith <casresm3@gmail.com>
2024-12-09 04:14:19 -0500

    Added modals to calendar

--- js/dashboardScript.js
+++ js/dashboardScript.js
@@ -3,12 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
   modalCalButtons.forEach((button) => {
     button.addEventListener("click", function () {
+        console.log("button click")
       const date = button.innerText;
 
-      const modal = document.querySelector(".calModal");
+      const targetModalId = button.getAttribute("data-bs-target");
+      const modal = document.querySelector(targetModalId);
+
       const modalTitle = modal.querySelector(".modal-title");
       if (modalTitle) {
+        console.log("in conditional", modalTitle)
         modalTitle.innerText = `December ${date}th`;
+        
       }
     });
   });
